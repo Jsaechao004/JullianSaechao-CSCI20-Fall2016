9b5958636c373ef38323a4f44525bc3bb532d890
Jullian Saechao <jsaechao004@student.butte.edu>
2016-08-29 22:53:29 +0000

    lab 1 folder updated for ascii art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,18 @@
+//Jullian Saechao
+//8/29/2016
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"  *" << endl;
+    cout <<" * *"<< endl;
+    cout <<" * *"<< endl;
+    cout <<" * *"<< endl;
+    cout <<" * *"<< endl;
+    cout <<"** **"<< endl;
+    cout <<" *-*"<< endl;
+    cout <<" *-*"<< endl;
+    cout <<" *-*"<< endl;
+}
