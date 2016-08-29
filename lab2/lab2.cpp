796f8710abb4ef81cc71f56057e27a48b8b03b66
Jullian Saechao <jsaechao004@student.butte.edu>
2016-08-29 23:55:36 +0000

    lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,11 @@
+//jullian saechao
+//8/29/2016
+
+/* 1. Computer choose a random number between 1-10.
+   2. The player starts with 10 points
+   3. The player then choose a number between 1-10.
+   4. If the player chooses an incorrect number, then they will lose 1 point.
+   5. If the number the player chooses is higher than the number the computer chooses, the program will tell them that it is lower.
+   6. If the number the player chooses is lower than the number the computer chooses, the program will tell them that it is higher.
+   7. If the player chooses the correct number, then they will be awarded with 10 points.
+   
