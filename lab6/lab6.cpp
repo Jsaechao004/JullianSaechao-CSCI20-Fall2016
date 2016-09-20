5e69edd595f58b015db1c054ee4be873fbe5e267
Jullian Saechao <jsaechao004@student.butte.edu>
2016-09-20 18:00:32 +0000

    Lab6 submission

---
+++ lab6/lab6.cpp
@@ -0,0 +1,25 @@
+//jullian Saechao
+// Lab 6 copyright function
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+main(){
+    string fName;
+    string lName;
+
+    cout << "Enter first name: " << endl;
+    cin >> fName;
+    
+    cout << "Enter last name: " << endl;
+    cin >> lName;
+    
+    cout << endl;
+    cout << endl; 
+    
+    cout << "(( _ _ ))..zzZZ" << " Copyright " << fName << " " << lName << " 2016.";
+    
+}
