72e11c379963f107eb23dc26896786d160148a20
Jullian Saechao <jsaechao004@student.butte.edu>
2016-09-20 20:44:14 +0000

    Lab7 submission

---
+++ lab7/lab7.cpp
@@ -0,0 +1,27 @@
+//Jullian Saechao
+//lab 7 Song
+#include <iostream>
+#include <string>
+using namespace std;
+
+void chorus(string num, string animal)
+{
+    
+    cout << num << " little " << animal << " jumping on the bed, " << endl;
+    cout << "One fell off and bumped his head, " << endl;
+    cout << "mama called the doctor," << endl;
+    cout << "and the doctor said, " << endl;
+    cout << "'No more monkeys jumping off the bed'" << endl;
+    
+    
+}
+    
+
+int main(){
+    chorus("5", "monkeys");
+    
+    cout << endl;
+    cout << endl;
+    
+    chorus("10", "wolves");
+}
